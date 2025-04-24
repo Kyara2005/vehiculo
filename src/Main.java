1be52e089f4dc84e72993480c1d8d8a852a13c1f
Kyara2005 <kyara.altamirano@epn.edu.ec>
2025-04-24 09:29:54 -0500

    proyecto

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Vehiculo v1 = new Vehiculo("hola", true);
+        Vehiculo v1 = new Vehiculo("Julia", "Azul", "Nissan");
+        System.out.println();
+        //Segundo
+        System.out.println("Ingrese datos nuevos: ");
+        System.out.println("Nombre: ");
+        String nombre1 = sc.nextLine();
+        System.out.println("Color: ");
+        String color1 = sc.nextLine();
+        System.out.println("Marca: ");
+        String marca1 = sc.nextLine();
+        Vehiculo v2 = new Vehiculo(nombre1, color1, marca1);
+
         v1.mostrar();
+        v2.mostrar();
 
     }
 }
