7c3509608985e19cd92d8a0fb1a90aea524ccd1d
Kyara2005 <kyara.altamirano@epn.edu.ec>
2025-04-24 09:19:06 -0500

    proyecto

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,13 +1,17 @@
 public class Vehiculo {
-    Float frenar;
+    String color;
     String nombre;
+    String marca;
 
-    public Vehiculo(String nombre, Float frenar) {
-        this.frenar = frenar;
+    public Vehiculo(String nombre, String color, String marca) {
+        this.color = color;
         this.nombre = nombre;
+        this.marca = marca;
     }
     public void mostrar() {
         System.out.println("Nombre: " + nombre);
-        System.out.println("Frenar: " + frenar);
+        System.out.println("Color: " + color);
+        System.out.println("Marca: " + marca);
     }
 }
+
