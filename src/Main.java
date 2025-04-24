8523550743e7a87674d7a2beb592aa0cea1647e3
POO5 <santiago.vargas@epn.edu.ec>
2025-04-24 09:19:53 -0500

    nuevo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 //Clase vehiculo, objeto frenar, mostrar información
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        Vehiculo v1 = new Vehiculo("hola", true);
+        v1.mostrar();
     }
 }
