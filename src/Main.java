fa303ed0968a2b5ab2858cb19be041b02b7534f2
POO5 <santiago.vargas@epn.edu.ec>
2025-04-24 09:33:40 -0500

    nuevo

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Vehiculo v1 = new Vehiculo("hola", true);
-        v1.mostrar();
+
 
     }
 }
