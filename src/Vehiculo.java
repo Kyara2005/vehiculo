ca8705cc132b34dd465f55cae1ccb99e7edb71d4
Ivanp2003 <andrespanchichavez@gmail.com>
2025-04-24 09:28:32 -0500

    prueba

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,11 +1,17 @@
 public class Vehiculo {
+
+    //Atributos
     Float frenar;
     String nombre;
 
+
+    //Constructor
     public Vehiculo(String nombre, Float frenar) {
         this.frenar = frenar;
         this.nombre = nombre;
     }
+
+    //Método
     public void mostrar() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Frenar: " + frenar);
